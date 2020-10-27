8c73aa047f006943ed70eca4c4f05b174fabc360
Philippe SJ <philippe.saint.jacques@gmail.com>
2020-10-27 20:16:47 +0100

    :construction_worker: add some global style

--- utils/GlobalStyle.js
+++ utils/GlobalStyle.js
@@ -28,6 +28,23 @@ img {
   border: 0px;
   width: 100%;
 }
+
+.arrow {
+  border: solid black;
+  border-width: 0 3px 3px 0;
+  display: inline-block;
+  padding: 3px;
+}
+
+.right {
+  transform: rotate(-45deg);
+  -webkit-transform: rotate(-45deg);
+}
+
+.left {
+  transform: rotate(135deg);
+  -webkit-transform: rotate(135deg);
+}
 `;
 
 export default GlobalStyle;
