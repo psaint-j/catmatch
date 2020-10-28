70cf0eb13f1980147f79340cb9a2549de368bf3a
Philippe SJ <philippe.saint.jacques@gmail.com>
2020-10-28 08:42:43 +0100

    :bug: bug fix - return unique random number

--- utils/func.js
+++ utils/func.js
@@ -1,9 +1,9 @@
 export const getRandomNumber = (max) => {
     const numberOne = Math.floor(Math.random() * max);
     let numberTwo = Math.floor(Math.random() * max); 
-    // while(numberOne == numberTwo) {
-    //     numberTwo = Math.floor(Math.random() * max);
-    // }
+    while(numberOne == numberTwo) {
+        numberTwo = Math.floor(Math.random() * max);
+    }
     return [
         numberOne,
         numberTwo
